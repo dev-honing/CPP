78e247306d1c89d9f86c70db13baed0ec497ba0d
Ho Nyeong <dev.honing@gmail.com>
2024-04-21 20:42:16 +0900

    Learn: 변수 선언과 출력 작성

--- main.cpp
+++ main.cpp
@@ -8,10 +8,35 @@
     JSDoc과 닮았지만, C++에서는 이렇게 작성한다.
 */
 
-// 2. 출력 함수
+// // 2. 출력 함수
+// #include <iostream>
+
+// int main() {
+//     std::cout << "Hello, C++!" << std::endl;
+//     return 0;
+// }
+
+// 3. 변수 선언
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, C++!" << std::endl;
+    // 3-1. 정수형 변수 선언과 초기화
+    int number = 10;
+    
+    // 3-2. 실수형 변수 선언
+    float float_number = 3.14f;
+
+    // 3-3. 문자형 변수 선언과 초기화
+    char letter = 'A';
+
+    // 3-4. 문자열 변수 선언과 초기화
+    std::string name = "BHN";
+
+    // 3-5. 변수 출력
+    std::cout << "정수형 변수: " << number << std::endl;
+    std::cout << "실수형 변수: " << float_number << std::endl;
+    std::cout << "문자형 변수: " << letter << std::endl;
+    std::cout << "문자열 변수: " << name << std::endl;
+
     return 0;
 }
