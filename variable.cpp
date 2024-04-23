78218e6235a2b6d67cdccf48980596360006009a
Ho Nyeong <dev.honing@gmail.com>
2024-04-24 00:01:29 +0900

    Learn: 변수의 선언과 할당, 초기화

--- variable.cpp
+++ variable.cpp
@@ -1,8 +1,14 @@
-// C++ 프로그램의 기본 틀
 #include <iostream>
+#include <string>
 
 int main(void) {
 
-	std::cout << "Hello, C++!" << std::endl;
-	return 0;
+    // 문자형과 문자열의 차이
+    int a; // 변수 선언
+    a = 10; // 할당(대입 연산자를 사용해 10이라는 값을 저장)
+    
+
+    int a = 10; // 변수 선언과 동시에 초기화
+
+    return 0;
 }
