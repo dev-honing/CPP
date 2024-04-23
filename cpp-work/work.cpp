b129eacb13137339023d2a0a7014fad1a3f4ae2b
Ho Nyeong <dev.honing@gmail.com>
2024-04-24 01:22:38 +0900

    Work: 문자열 출력 점검
    - 한 줄로 출력
    - 여러 줄로 출력

---
+++ cpp-work/work.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void) {
+	// 한 줄로 출력
+	std::cout << "안녕하세요~ " << "C++ 프로그래밍에 입문한 주니어 개발자입니다!" << std::endl;
+
+	// \n을 사용하여 여러 줄로 출력
+	std::cout << "안녕하세요~\n" << "C++ 프로그래밍에 입문한 주니어 개발자입니다!" << std::endl;
+
+	return 0;
+}
