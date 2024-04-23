66e7e38c7661bfebbbac5ba63662804c74b17e7a
Ho Nyeong <dev.honing@gmail.com>
2024-04-24 02:06:31 +0900

    Work: 문제 6번 해결

--- cpp-work/variable.cpp
+++ cpp-work/variable.cpp
@@ -2,19 +2,31 @@
 #include <string>
 
 int main(void) {
-	// 문제 5. 덧셈 구현 - 변수 a, b를 선언하고 a에는 10, b에는 2를 할당한 뒤, a+b를 출력하시오.
-	
-	// 5-1. 변수 a, b 선언
+	// 문제 6. 계산기 구현 - 변수 a, b를 선언하고 a에는 100, b에는 10을 할당하고 다음을 출력하시오.
+	/*
+	* 
+	1) a + b
+
+	2) a - b
+
+	3) a * b
+
+	4) a / b
+	*/
+
+	// 6-1. 변수 선언
 	int a;
 	int b;
 
-	// 5-2. 값을 할당
-	a = 10;
-	b = 2;
+	// 6-2. 값 할당
+	a = 100;
+	b = 10;
 
-	// 5-3. a+b를 출력
-	std::cout << "정수 a와 b의 합은 " << a + b << "입니다." << std::endl;
-
+	// 6-3. 계산기 구현
+	std::cout << "a + b = " << a + b << std::endl;
+	std::cout << "a - b = " << a - b << std::endl;
+	std::cout << "a * b = " << a * b << std::endl;
+	std::cout << "a / b = " << a / b << std::endl;
 
 	return 0;
 }
