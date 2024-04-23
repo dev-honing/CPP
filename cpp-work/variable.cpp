c6a2ee2a480177bd8695345d8b29455308c65d46
Ho Nyeong <dev.honing@gmail.com>
2024-04-24 01:41:42 +0900

    Work: 3번 문제 해결

--- cpp-work/variable.cpp
+++ cpp-work/variable.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <string>
 
 int main(void) {
-	// 문제 2. 네임스페이스를 사용하여 std::를 생략하고 “Hello, C++!”를 출력하시오.
-	
-	// 2-1. 생략할 네임 스페이스를 명시
-	using namespace std;
+	// 문제 3. 띄어쓰기 구현 - 변수를 선언하고 가운데 공백을 반영하여 출력하시오.
+	std::string date = "2024-04-24"; // date 변수에 날짜 데이터를 선언 및 초기화
+	std::string day = "수"; // day 변수에 요일 데이터를 선언 및 초기화
 
-	// 2-2. std::를 생략하고 출력
-	cout << "Hello, C++!" << endl;
+	// 변수에 저장된 데이터를 출력
+	std::cout << "오늘의 날짜는 " << date << "이고, 오늘은 " << day << "요일입니다." << std::endl;
 
 	return 0;
 }
