be63882d624c6ad3e09924646b104595deaf7922
Ho Nyeong <dev.honing@gmail.com>
2024-04-24 14:40:31 +0900

    Work: 문제 12번 해결

--- cpp-work/work.cpp
+++ cpp-work/work.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 
 int main() {
-	// 문제 11. “오늘 마신 커피는 몇 잔인가요?”라는 질문을 출력하고 cin을 사용하여 오늘 마신 커피가 몇 잔인지 입력하시오. 그리고 사용자가 입력한 정보를 변수에 담아, 적정량에 대한 임의의 기준에 따라 메시지를 출력하시오.
+	// 문제 12. a, b 변수 두 개를 입력받고, 사칙연산을 출력하시오.
+	
+	// 12-1. 안내 메시지 출력
+	std::cout << "사용할 변수 a, b를 입력하세요." << std::endl;
 
-	// 11-1. 질문지를 콘솔 출력
-	std::cout << "오늘 마신 커피는 몇 잔인가요?" << std::endl;
+	// 12-2. 변수 선언
+	int a;
+	int b;
 
-	// 11-2. 오늘 마신 커피를 담을 todayCoffee를 변수로 선언
-	int todayCoffee;
+	// 12-3. a 값 입력받기
+	std::cout << "사용할 변수 a의 값은?" << std::endl;
+	std::cin >> a;
 
-	// 11-3. 사용자 입력 및 변수에 저장
-	std::cin >> todayCoffee;
+	// 12-4. b 값 입력받기 
+	std::cout << "사용할 변수 b의 값은?" << std::endl;
+	std::cin >> b;
 
-	// 11-4. 조건문 작성
-	if (todayCoffee >= 3) {
-		std::cout << "너무 많이 마셨어요. 더이상 마시면 몸에 해롭습니다." << std::endl;
-	}
-	else {
-		if (todayCoffee == 2) {
-			std::cout << "적당량의 커피를 마셨네요. 훌륭합니다!" << std::endl;
-		}
-		else {
-			if (todayCoffee == 1) {
-				std::cout << "오늘은 컨디션이 매우 좋군요. 좋은 하루 보내세요!" << std::endl;
-			}
-		}
+	// 12-5. 입력받은 값 출력
+	std::cout << "변수 a의 값은 " << a << "이라고 입력하셨습니다." << std::endl;
+	std::cout << "변수 b의 값은 " << b << "이라고 입력하셨습니다." << std::endl;
 
-	}
-
-
+	// 12-6. 사칙연산 출력
+	std::cout << "a + b = " << a + b << std::endl;
+	std::cout << "a - b = " << a - b << std::endl;
+	std::cout << "a * b = " << a * b << std::endl;
+	std::cout << "a / b = " << a / b << std::endl;
 
 	return 0;
 }
+
+// 콘솔 출력 내용
+/*
+변수 a의 값은 10이라고 입력하셨습니다.
+변수 b의 값은 5이라고 입력하셨습니다.
+a + b = 15
+a - b = 5
+a * b = 50
+a / b = 2
+*/
