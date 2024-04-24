f02902feedd930a497fd101860ccb4c59fbccf77
Ho Nyeong <dev.honing@gmail.com>
2024-04-24 20:43:20 +0900

    Question: 연산자 오버로딩 함수가 뭔지 모르겠다
    - 객체 자체를 출력하는 것은 성공

--- cpp-struct/struct.cpp
+++ cpp-struct/struct.cpp
@@ -9,6 +9,12 @@ struct Person {
 	std::string interest;
 };
 
+// << 연산자 오버로딩 함수 정의
+std::ostream& operator<<(std::ostream& os, const Person& person) {
+	os << "Name: " << person.name << ", Birth Year: " << person.birthYear << ", Age: " << person.age << ", Interest: " << person.interest;
+	return os;
+}
+
 int main(void) {
 	// 구조체 변수 생성 및 초기화
 	Person Ho;
@@ -22,11 +28,8 @@ int main(void) {
 	* name, age, birthYear, interest: 멤버 변수
 	*/
 
-	// Ho 객체의 각 멤버 변수 출력
-	std::cout << "Ho의 이름은 " << Ho.name << std::endl;
-	std::cout << "Ho의 출생연도는 " << Ho.birthYear << std::endl;
-	std::cout << "Ho의 나이는 " << Ho.age << std::endl;
-	std::cout << "Ho의 관심사는 " << Ho.interest << std::endl;
+	// Ho 객체 출력
+	std::cout << "Person 객체의 형태는 다음과 같습니다.\n" << Ho << std::endl;
 
 	return 0;
 
