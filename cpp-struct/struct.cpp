7dde54510478d380980c894d55aff0bb66646047
Ho Nyeong <dev.honing@gmail.com>
2024-04-24 19:35:38 +0900

    Learn: Ho 객체의 각 멤버 변수를 출력

--- cpp-struct/struct.cpp
+++ cpp-struct/struct.cpp
@@ -17,4 +17,17 @@ int main(void) {
 	Ho.age = 26;
 	Ho.interest = "C++";
 
+	/*
+	* Ho: Person 구조체의 인스턴스, Person 인스턴스(객체)
+	* name, age, birthYear, interest: 멤버 변수
+	*/
+
+	// Ho 객체의 각 멤버 변수 출력
+	std::cout << "Ho의 이름은 " << Ho.name << std::endl;
+	std::cout << "Ho의 출생연도는 " << Ho.birthYear << std::endl;
+	std::cout << "Ho의 나이는 " << Ho.age << std::endl;
+	std::cout << "Ho의 관심사는 " << Ho.interest << std::endl;
+
+	return 0;
+
 }
